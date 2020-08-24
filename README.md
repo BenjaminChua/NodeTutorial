1d21cf918d4efff31a3afddadf2ebddf5268884c
Benjamin Chua <benjaminchua@AISG-MacBook-Pro.local>
2020-08-24 22:32:57 +0800

    Example of callback function

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Nodejs, Express and MongoDB
 
+1. Define a node module
+    - rectangle.js
+        ```
+        exports.area = (x, y) => (x*y);
+        ```
+    - index.js
+        ```
+        var rect = require('./rectangle')
+        rect.area(x, y)
+        ```
+2. Define a callback function
--- node-examples/index.js
+++ node-examples/index.js
@@ -1,15 +1,19 @@
-var rect = require('./rectangle')
+var rect = require('./rectangle');
+const rectangle = require('./rectangle');
 
 solveRect = (l, b) => {
     console.log(`Solving for rectangle with l = ${l} and b = ${b}`)
 
-    if (l <= 0 || b <= 0) {
-        console.log(`Rectangle dimensions should be greater than zero: l = ${l} and b = ${b}`)
-    }
-    else {
-        console.log(`The area of the rectangle is ${rect.area(l, b)}`);
-        console.log(`The perimeter of the rectangle is ${rect.perimeter(l, b)}`);
-    }
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log(`ERROR: ${err.message}`)
+        }
+        else {
+            console.log(`The area of the rectangle of dimensions l = ${l} and b = ${b} is ${rectangle.area()}`)
+            console.log(`The perimeter of the rectangle of dimensions l = ${l} and b = ${b} is ${rectangle.perimeter()}`)
+        }
+    });
+    console.log("This statement is after the call to rect()");
 }
 
 solveRect(2, 4);
