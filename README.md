3caae5cd14f6f9c860ab137f7a36ef5ce4bf907c
Benjamin Chua <benjaminchua@AISG-MacBook-Pro.local>
2020-08-30 18:45:17 +0800

    express router

--- README.md
+++ README.md
@@ -113,7 +113,7 @@
             console.log(`Server running at http://${hostname}:${port}`)
         });
         ```
-6. Express.js REST API and Body Parser
+6. Express.js REST API & Body Parser
     - index.js
         ```
         app.use(bodyParser.json());
@@ -131,4 +131,26 @@
             res.write(`Updating the dish: ${req.params.dishID} \n`);
             res.end(`Will update the dish: ${req.body.name} with details: ${req.body.description}`);
         });
+        ```
+7. Express.js REST API, Body Parser & Express Router
+    - index.js
+        ```
+        
+        ```
+    - dishRouter.js
+        ```
+        const dishRouter = express.Router();
+        dishRouter.use(bodyParser.json());
+
+        dishRouter.route('/')
+            .all((req, res, next) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/plain');
+                next();
+            })
+            .get((req, res, next) => {
+                res.end('Will send all the dishes to you!');
+            });
+
+        module.exports = dishRouter;
         ```
--- node-express/index.js
+++ node-express/index.js
@@ -3,6 +3,9 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+// import routes from /routes
+const dishRouter = require('./routes/dishRouter');
+
 const hostname = 'localhost';
 const port = 3000;
 
@@ -10,47 +13,10 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
+// mount routes
+app.use('/dishes', dishRouter);
 
-app.get('/dishes', (req, res, next) => {
-    res.end('Will send all the dishes to you!');
-});
-
-app.post('/dishes', (req, res, next) => {
-    res.end(`Will add the dish: ${req.body.name} with details: ${req.body.description}`);
-});
-
-app.put('/dishes', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-});
-
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all the dishes!');
-});
-
-app.get('/dishes/:dishID', (req, res, next) => {
-    res.end(`Will send details of the dish: ${req.params.dishID} to you!`);
-});
-
-app.post('/dishes/:dishID', (req, res, next) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /dishes/${req.params.dishID}`);
-});
-
-app.put('/dishes/:dishID', (req, res, next) => {
-    res.write(`Updating the dish: ${req.params.dishID} \n`);
-    res.end(`Will update the dish: ${req.body.name} with details: ${req.body.description}`);
-});
-
-app.delete('/dishes/:dishID', (req, res, next) => {
-    res.end(`Deleting dish: ${req.params.dishID}`);
-});
-
+// serve static pages in /public
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
