673896d49f6b974a495a0232d4c20a2921eeb2b2
Benjamin Chua <boyz_pro95@hotmail.com>
2020-09-06 09:10:31 +0800

    Resolved callback hell using promise

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,46 +5,44 @@ const dboper = require('./operations.js');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err, client) => {
-    
-    assert.equal(err, null);
-
+MongoClient
+.connect(url)
+.then((client) => {
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
 
-    dboper.insertDocument(
-        db, 
-        { 
-            name: 'Vadonut', 
-            description: 'Test'
-        },
-        'dishes',
-        (result) => {
-            console.log("Insert document:\n", result.ops);
+    dboper
+    .insertDocument(db, { name: 'Vadonut', description: 'Test' }, 'dishes')
+    .then((result) => {
+        console.log("Insert document:\n", result.ops);
 
-            dboper.findDocuments(db, 'dishes', (docs) => {
-                console.log("Found documents:\n", docs);
+        return dboper.findDocuments(db, 'dishes')
+    })
+    .then((docs) => {
+        console.log("Found documents:\n", docs);
 
-                dboper.updateDocument(
-                    db, 
-                    { name: 'Vadonut' },
-                    { description: 'Updated Test' },
-                    'dishes',
-                    (result) => {
-                        console.log("Updated document:\n", result.result);
-                        dboper.findDocuments(db, 'dishes', (docs) => {
-                            console.log("Found documents:\n", docs);
+        return dboper.updateDocument(
+            db, 
+            { name: 'Vadonut' },
+            { description: 'Updated Test' },
+            'dishes')
+    })
+    .then((result) => {
+        console.log("Updated document:\n", result.result);
+        
+        return dboper.findDocuments(db, 'dishes')
+    })
+    .then((docs) => {
+        console.log("Found documents:\n", docs);
 
-                            db.dropCollection('dishes', (result) => {
-                                console.log("Dropped collection:\n", result);
+        return db.dropCollection('dishes')
+    })
+    .then((result) => {
+        console.log("Dropped collection:\n", result);
 
-                                client.close();
-                            });
-                        });
-                    }
-                    );
-            });
-        }
-        );
-});
+        client.close();
+    })
+    .catch((err) => console.log(err));
+})
+.catch((err) => console.log(err));
--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -4,47 +4,24 @@ const assert = require('assert');
 
 exports.insertDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.insert(document, (err, result) => {
-        assert.equal(err, null);
-        console.log(`Inserted ${result.result.n} documents into the collection ${collection}`);
-        
-        // return callback to calling function
-        callback(result);
-    });
+    return coll.insert(document);
 };
 
 exports.findDocuments = (db, collection, callback) => {
     const coll = db.collection(collection);
     
     // findAll using empty {}
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-        
-        // return callback to calling function        
-        callback(docs);
-    });
+    return coll.find({}).toArray();
 };
 
 exports.removeDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        assert.equal(err, null);
-        console.log(`Removed the document ${document}`);
-
-        // return callback to calling function        
-        callback(result);    
-    });
+    return coll.deleteOne(document);
 };
 
 exports.updateDocument = (db, document, update, collection, callback) => {
     const coll = db.collection(collection);
 
     // second param is the field that needs to be updated
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        assert.equal(err, null);
-        console.log(`Updated the document with ${update}`);   
-        
-        // return callback to calling function
-        callback(result);
-    });
+    return coll.updateOne(document, { $set: update }, null);
 };
