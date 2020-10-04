dffaa6842df1caf201c6a32dbe3f8e29316bfc27
Benjamin Chua <boyz_pro95@hotmail.com>
2020-10-04 15:44:15 +0800

    change error to 403

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -52,7 +52,7 @@ function auth (req, res, next) {
 
 	if (!req.session.user) {
 		var err = new Error('You are not authenticated!');
-		err.status = 401;
+		err.status = 403;
 		return next(err);
 	}
 	else {
